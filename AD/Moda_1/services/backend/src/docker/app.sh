e90f4c08b2fecc921cff512796c8c64ba1b0153c
icYFTL <savap0@yandex.ru>
2024-02-25 20:55:00 +0300

    Moda fix

--- AD/Moda_1/services/backend/src/docker/app.sh
+++ AD/Moda_1/services/backend/src/docker/app.sh
@@ -5,4 +5,6 @@ alembic upgrade head
 
 cd src
 
+mv /usr/local/lib/python3.10/site-packages/fastapi_users/authentication/strategy/jwt_.py /usr/local/lib/python3.10/site-packages/fastapi_users/authentication/strategy/jwt.py
+
 gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:3000
